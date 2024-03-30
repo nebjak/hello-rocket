096b52656f12c4825907ea896e700b74fb648238
Nebojsa Jakovljevic <nebojsa@nebjak.net>
2024-03-30 08:28:52 +0100

    Create Utils module and healthcheck route (#1)

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,6 +1,9 @@
 use crate::home::controllers::home;
+use crate::utils::controllers::healthcheck;
 use rocket::{Build, Rocket};
 
 pub fn deploy() -> Rocket<Build> {
-    rocket::build().mount("/", routes![home::index])
+    rocket::build()
+        .mount("/", routes![home::index])
+        .mount("/healthcheck", routes![healthcheck::index])
 }
---
+++ src/utils/services/healthcheck.rs
@@ -0,0 +1,3 @@
+pub fn run_healthcheck() -> &'static str {
+    "OK"
+}
